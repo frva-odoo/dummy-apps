c6f26f78978b98e026fa9c364c50584e17947867
Francois Vasamillet (frva) <frva@odoo.com>
2026-07-22 16:34:52 +0200

    [ADD] st_bird: add bird module

---
+++ st_bird/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'ST Bird',
+    'version': '1.0',
+    'category': 'Accounting',
+    'summary': 'The base folder for the birds',
+    'description': "A dummy module that is used as a basis for every bird",
+    'website': '',
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'assets': {{}},
+    'price': 40.00,
+    'currency': 'EUR',
+}
